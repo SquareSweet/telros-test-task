6e2b6b319f5ef383fedb59108cffd263f0828b7a
Svetlana Antoshkina <svetlana120595@gmail.com>
2023-05-17 17:44:25 +0300

    Initial commit

---
+++ src/main/java/prj/sqsw/telrostest/TelrosTestApplication.java
@@ -0,0 +1,13 @@
+package prj.sqsw.telrostest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelrosTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelrosTestApplication.class, args);
+	}
+
+}
